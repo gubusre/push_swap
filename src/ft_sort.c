96ea95f1f105565377d028a2b31a7a5426847dda
gubusre <g96busquets@gmail.com>
2025-11-18 15:34:58 +0100

    Add files via upload

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -6,77 +6,109 @@
 /*   By: gubusque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 18:34:20 by gubusque          #+#    #+#             */
-/*   Updated: 2025/11/16 18:38:30 by gubusque         ###   ########.fr       */
+/*   Updated: 2025/11/17 15:57:12 by gubusque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int get_pivot(int *arr, int size)
+static int	get_pivot(int *arr, int size)
 {
-    int *copy = malloc(size * sizeof(int));
-    if (!copy) return 0;
-    for (int i = 0; i < size; i++) copy[i] = arr[i];
-    for (int i = 0; i < size - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < size; j++)
-            if (copy[j] < copy[min_idx])
-                min_idx = j;
-        int temp = copy[i];
-        copy[i] = copy[min_idx];
-        copy[min_idx] = temp;
-    }
-    int pivot = copy[size / 2];
-    free(copy);
-    return pivot;
+	int	*copy;
+	int	min_idx;
+	int	pivot;
+	int	temp;
+	int	i;
+	int	j;
+
+	copy = malloc(size * sizeof(int));
+	if (!copy)
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		copy[i] = arr[i];
+		i++;
+	}
+	i = 0;
+	while (i < size - 1)
+	{
+		min_idx = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (copy[j] < copy[min_idx])
+			min_idx = j;
+			j++;
+		}
+		temp = copy[i];
+		copy[i] = copy[min_idx];
+		copy[min_idx] = temp;
+		i++;
+	}
+	pivot = copy[size / 2];
+	free(copy);
+	return (pivot);
 }
 
-static void small_sort(t_stacks *s)
+static void	small_sort(t_stacks *s)
 {
-    if (s->size_a == 2 && s->a[0] > s->a[1])
-        sa(s);
-    else if (s->size_a == 3) {
-        while (!check_is_sorted(s)) {
-            if (s->a[0] > s->a[1] && s->a[0] > s->a[2])
-                ra(s);
-            else if (s->a[1] > s->a[2])
-                rra(s);
-            else
-                sa(s);
-        }
-    }
+	if (s->size_a == 2 && s->a[0] > s->a[1])
+		sa(s);
+	else if (s->size_a == 3)
+	{
+		while (!check_is_sorted(s))
+		{
+			if (s->a[0] > s->a[1] && s->a[0] > s->a[2])
+				ra(s);
+			else if (s->a[1] > s->a[2])
+				rra(s);
+			else
+				sa(s);
+		}
+	}
 }
 
-static void quicksort_rec(t_stacks *s, int size)
+static void	quicksort_rec(t_stacks *s, int size)
 {
-    if (size <= 3) {
-        small_sort(s);
-        return;
-    }
+	int	pivot;
+	int	pushed;
+	int	i;
 
-    int pivot = get_pivot(s->a, size);
-    int pushed = 0;
-
-    for (int i = 0; i < size; i++) {
-        if (s->a[0] <= pivot) {
-            pb(s);
-            pushed++;
-        } else {
-            ra(s);
-        }
-    }
-
-    for (int i = 0; i < size - pushed; i++)
-        rra(s);
-    quicksort_rec(s, pushed);
-    quicksort_rec(s, size - pushed);
-    while (s->size_b > 0)
-        pa(s);
+	if (size <= 3)
+	{
+		small_sort(s);
+		return	;
+	}
+	pivot = get_pivot(s->a, size);
+	pushed = 0;
+	i = 0;
+	while (i < size)
+	{
+		if (s->a[0] <= pivot)
+		{
+			pb(s);
+			pushed++;
+		}
+		else
+			ra(s);
+		i++;
+	}
+	i = 0;
+	while (i < size - pushed)
+	{
+		rra(s);
+		i++;
+	}
+	quicksort_rec(s, pushed);
+	quicksort_rec(s, size - pushed);
+	while (s->size_b > 0)
+		pa(s);
 }
 
-void ft_push_swap_sort(t_stacks *s)
+void	ft_push_swap_sort(t_stacks *s)
 {
     if (check_is_sorted(s))
-        return;
+        return	;
     quicksort_rec(s, s->size_a);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gubusque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 18:36:01 by gubusque          #+#    #+#             */
-/*   Updated: 2025/11/16 18:36:06 by gubusque         ###   ########.fr       */
+/*   Updated: 2025/11/17 15:50:55 by gubusque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,41 @@
 
 int main(int argc, char *argv[])
 {
-    t_stacks s = {0};
+	t_stacks s = {0};
+	int	i;
 
-    if (argc < 2) {
-        write(2, "Error\n", 6);
-        return (1);
-    }
-
-    s.size_a = argc - 1;
-    s.a = malloc(s.size_a * sizeof(int));
-    s.b = malloc(s.size_a * sizeof(int));
-    if (!s.a || !s.b) return (1);
-
-    for (int i = 0; i < s.size_a; i++) {
-        if (!check_is_valid_number(argv[i + 1])) {
-            write(2, "Error\n", 6);
-            free(s.a); free(s.b);
-            return (1);
-        }
-        s.a[i] = ft_atoi(argv[i + 1]);
-    }
-
-    if (check_has_duplicates(s.a, s.size_a)) {
-        write(2, "Error\n", 6);
-        free(s.a); free(s.b);
-        return (1);
-    }
-
-    ft_push_swap_sort(&s);
-
-    free(s.a);
-    free(s.b);
-    return (0);
+	if (argc < 2)
+	{
+		write(2, "Error\n", 6);
+		return (1);
+	}
+	s.size_a = argc - 1;
+	s.a = malloc(s.size_a * sizeof(int));
+	s.b = malloc(s.size_a * sizeof(int));
+	if (!s.a || !s.b)
+		return (1);
+	i = 0;
+	while (i < s.size_a)
+	{
+		if (!check_is_valid_number(argv[i + 1]))
+		{
+			write(2, "Error\n", 6);
+			free(s.a);
+			free(s.b);
+			return (1);
+		}
+		s.a[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
+	if (check_has_duplicates(s.a, s.size_a))
+	{
+		write(2, "Error\n", 6);
+		free(s.a);
+		free(s.b);
+		return (1);
+	}
+	ft_push_swap_sort(&s);
+	free(s.a);
+	free(s.b);
+	return (0);
 }
